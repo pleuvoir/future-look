597f219354b33326ff6e69c38fabdc61b8cb5fd9
pleuvoir <pleuvior@foxmail.com>
2018-08-14 00:36:39 +0800

    :blush: init scheduler-example

---
+++ code/scheduler-example/src/main/java/io/github/pleuvoir/quartz/QuartzConst.java
@@ -0,0 +1,18 @@
+package io.github.pleuvoir.quartz;
+
+public class QuartzConst {
+
+	public static class JobKeyGroup{
+
+		public static final String CLASS = "class";
+
+		public static final String USER_CHECK = "userCheck";
+	}
+
+	public static class TriggerKeyGroup{
+
+		public static final String CLASS = "class";
+
+		public static final String USER_CHECK = "userCheck";
+	}
+}
